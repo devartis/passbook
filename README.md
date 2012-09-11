c352d966fb78cde8e349c7bec553814d4104f767
Fernando Aramendi <fernando@devartis.com>
2012-09-11 12:13:56 -0300

    Created pass model and json serializers

--- README.md
+++ README.md
@@ -8,11 +8,28 @@ Typical usage often looks like this::
 
     #!/usr/bin/env python
 
-    from passbook.models import Pass
+    from passbook.models import Pass, Barcode, BoardingPass
 
-    passfile = Pass()
-    passfile.JSON = '{...}' # pass.json contents
-    passfile.addFile('images/icon.png') # attached images
+    cardInfo = BoardingPass()
+    cardInfo.addPrimaryField('origin', 'EZE', 'Buenos Aires')
+    cardInfo.addPrimaryField('destination', 'MDQ', 'Mar del Plata')
+    cardInfo.addSecondaryField('gate', 'F12', 'Gate')
+    cardInfo.addSecondaryField('date', '08/11/2012 10:22', 'Departure date')
+    cardInfo.addBackField('passenger-name', 'Fernando Aramendi', 'Passenger')
+    
+    passfile = Pass(cardInfo)
+    passfile.description = 'Demo pass' 
+    passfile.organizationName = 'devartis' 
+    passfile.passTypeIdentifier = 'pass.com.devartis.test' 
+    passfile.serialNumber = '12345678' 
+    passfile.teamIdentifier = 'AGK5BZEN3E'
+    passfile.backgroundColor = 'rgb(107,156,196)', 
+    passfile.logoText = 'devartis', 
+    passfile.barcode = Barcode(message = 'Flight-GateF12-ID6643679AH7B')    
+    
+    passfile.addFile('images/icon.png')
+    passfile.addFile('images/icon@2x.png')
+    passfile.addFile('images/logo.png')
     passfile.create('certificate.pem', 'key.pem', '123456') # Create and output the Passbook file (.pkpass) 
 
 
--- passbook/models.py
+++ passbook/models.py
@@ -1,46 +1,157 @@
 import json
+import simplejson
 import hashlib
 import zipfile
 import base64
 import os.path
 from M2Crypto import SMIME
 
+
+class Field:
+
+    def __init__(self, key = '', value = '', label = ''):
+
+        self.key = key
+        self.label = label
+        self.value = value
+
+    def json_dict(self):
+        return self.__dict__
+        
+        
+class Barcode:
+
+    def __init__(self, format = 'PKBarcodeFormatQR', message = '', messageEncoding = 'iso-8859-1'):
+
+        self.format = format
+        self.message = message
+        self.value = messageEncoding
+
+    def json_dict(self):
+        return self.__dict__
+        
+
+class PassInformation(object):
+    
+    def __init__(self):
+        self.primaryFields = []
+        self.secondaryFields = []
+        self.backFields = []
+
+    def addPrimaryField(self, key, value, label):
+        self.primaryFields.append(Field(key, value, label))
+
+    def addSecondaryField(self, key, value, label):
+        self.secondaryFields.append(Field(key, value, label))
+
+    def addBackField(self, key, value, label):
+        self.backFields.append(Field(key, value, label))
+
+        
+    def json_dict(self):
+        return {
+                'primaryFields' : [f.json_dict() for f in self.primaryFields],
+                'secondaryFields' : [f.json_dict() for f in self.secondaryFields],
+                'backFields' : [f.json_dict() for f in self.backFields],
+               }
+        
+        
+class BoardingPass(PassInformation):
+
+    def __init__(self, transitType = 'PKTransitTypeAir'):
+        super(BoardingPass, self).__init__()
+        self.transitType = transitType # PKTransitTypeAir, PKTransitTypeTrain, PKTransitTypeBus, PKTransitTypeBoat, PKTransitTypeGeneric.
+        self.jsonname = 'boardingPass'
+
+    def json_dict(self):
+        d = super(BoardingPass, self).json_dict()
+        d.update({'transitType': self.transitType})
+        return d
+        
+class Coupon(PassInformation):
+
+    def __init__(self):
+        super(Coupon, self).__init__()
+        self.jsonname = 'coupon'
+
+    
+class EventTicket(PassInformation):
+
+    def __init__(self):
+        super(EventTicket, self).__init__()
+        self.jsonname = 'event_ticket'
+
+    
+class Generic(PassInformation):
+
+    def __init__(self):
+        super(Generic, self).__init__()
+        super(Generic, self).__init__()
+        self.jsonname = 'store_card'
+
+    
+class StoreCard(PassInformation):
+
+    def __init__(self):
+        super(StoreCard, self).__init__()
+        self.jsonname = 'store_card'
+
+    
 class Pass :
     
-    files = [] # Holds the files to include in the .pkpass    
-    JSON = '' # Holds the json    
-    SHAs = {} # Holds the SHAs of the files array
+    def __init__(self, passInformation, json = ''):
 
-    def __init__(self, JSON = ''):
+        self._json = json
+        self._files = [] # Holds the files to include in the .pkpass    
+        self._json = '' # Holds the json    
+        self._hashes = {} # Holds the SHAs of the files array        
+        
+        self.description = '' # Required. Brief description of the pass, used by the iOS accessibility technologies.   
+        self.formatVersion = 1 # Required. Version of the file format. The value must be 1.
+        self.organizationName = '' # Required. Display name of the organization that originated and signed the pass.
+        self.passTypeIdentifier = '' # Required. Pass type identifier, as issued by Apple. The value must correspond with your signing certificate.        
+        self.serialNumber = '' # Required. Serial number that uniquely identifies the pass. 
+        self.teamIdentifier = '' # Required. Team identifier of the organization that originated and signed the pass, as issued by Apple.
 
-        self.JSON = JSON
-    
+        self.backgroundColor = 'rgb(255,255, 255)'
+        self.logoText = ''
+        self.barcode = None
+        
+        self.passInformation = passInformation        
+        
         
     # Adds file to the file array
     def addFile(self, path):
 
         if os.path.exists(path):
-            self.files.append(path)
+            self._files.append(path)
 
     
     # Creates the actual .pkpass file
     def create(self, certificate, key, password):
 
+        self._json = self.createPassJson()
+        print self._json
         manifest = self.createManifest()
         self.createSignature(manifest, certificate, key, password)
         self.createZip(manifest)
         self.clean()
         
     
+    def createPassJson(self):
+
+        return json.dumps(self, default=PassHandler)
+
+    
     # creates the hashes for the files and adds them into a json string.
     def createManifest(self):
 
         # Creates SHA hashes for all files in package
-        self.SHAs['pass.json'] = hashlib.sha1(self.JSON).hexdigest()
-        for filename in self.files :
-            self.SHAs[os.path.basename(filename)] = hashlib.sha1(open(filename).read()).hexdigest()
+        self._hashes['pass.json'] = hashlib.sha1(self._json).hexdigest()
+        for filename in self._files :
+            self._hashes[os.path.basename(filename)] = hashlib.sha1(open(filename).read()).hexdigest()
         
-        manifest = json.dumps(self.SHAs)
+        manifest = json.dumps(self._hashes)
         
         return manifest
     
@@ -71,11 +182,34 @@ class Pass :
         zf = zipfile.ZipFile('pass.pkpass', 'w')
         zf.write('signature', 'signature')
         zf.writestr('manifest.json', manifest)
-        zf.writestr('pass.json', self.JSON)
-        for filename in self.files:
+        zf.writestr('pass.json', self._json)
+        for filename in self._files:
             zf.write(filename, os.path.basename(filename))
         zf.close()
         
     def clean(self):
         os.remove('signature')
         os.remove('manifest.json')
+
+    def json_dict(self):
+
+        return {'description': self.description,
+                'formatVersion': self.formatVersion,
+                'organizationName': self.organizationName,
+                'passTypeIdentifier': self.passTypeIdentifier,
+                'serialNumber': self.serialNumber,
+                'teamIdentifier': self.teamIdentifier,
+                'backgroundColor': self.backgroundColor,
+                'logoText': self.logoText,
+                'barcode': self.barcode.json_dict(),
+                self.passInformation.jsonname: self.passInformation.json_dict()
+               }
+
+
+def PassHandler(obj):
+    if hasattr(obj, 'json_dict'):
+        return obj.json_dict()
+    else:
+        raise TypeError, 'Object of type %s with value of %s is not JSON serializable' % (type(obj), repr(obj))
+
+        
